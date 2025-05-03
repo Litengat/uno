29bf18b37bf653db8a2052c80cfe53a1c95b28bb
Litengat <Litengut@icloud.com>
2025-05-04 00:10:11 +0200

    fix: update API URL in CreateGame component to use environment variable

--- src/createGame.tsx
+++ src/createGame.tsx
@@ -1,15 +1,18 @@
 import { useNavigate } from "react-router";
 import { Button } from "./components/ui/button";
 
+const url = import.meta.env.VITE_API_URL;
+const create = `${url}/create`;
+
 export function CreateGame() {
   let navigate = useNavigate();
   return (
     <>
       <Button
         onClick={async () => {
-          const id = (await (
-            await fetch("http://localhost:5173/create")
-          ).json()) as unknown as { id: string };
+          const id = (await (await fetch(create)).json()) as unknown as {
+            id: string;
+          };
           console.log("Game created with ID:", id.id);
           navigate(`/game/${id.id}`);
         }}
