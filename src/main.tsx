bbe41b17e2fe387672c26a166b9aee5f7c1ef373
Litengat <Litengut@icloud.com>
2025-05-30 12:44:55 +0200

    undo nuke

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,10 @@ import App from "./App.tsx";
 import { CreateGame } from "./createGame.tsx";
 import { CardTest } from "./Cardtest.tsx";
 import { Game } from "./Game.tsx";
+<<<<<<< HEAD
 import SignIn from "./sign-in.tsx";
+=======
+>>>>>>> parent of 5788069 (nuke)
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -18,7 +21,10 @@ createRoot(document.getElementById("root")!).render(
           <Route path="/create" element={<CreateGame />} />
           <Route path="/cardtest" element={<CardTest />} />
           <Route path="/game/:id" element={<Game />} />
+<<<<<<< HEAD
           <Route path="signin" element={<SignIn />} />
+=======
+>>>>>>> parent of 5788069 (nuke)
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
--- worker/worker.ts
+++ worker/worker.ts
@@ -1,5 +1,6 @@
 import { Env } from "./index";
 
+<<<<<<< HEAD
 import { Hono } from "hono";
 
 const app = new Hono<{ Bindings: Env }>();
@@ -43,3 +44,62 @@ app.get("/create", (c) => {
 });
 
 export default app;
+=======
+export default {
+  async fetch(
+    request: Request,
+    env: Env,
+    _ctx: ExecutionContext
+  ): Promise<Response> {
+    const url = new URL(request.url);
+    const path = url.pathname.split("/").filter(Boolean);
+
+    if (path[0] === "websocket") {
+      console.log(new URL(request.url).pathname);
+      // Expect to receive a WebSocket Upgrade request.
+      // If there is one, accept the request and return a WebSocket Response.
+      const upgradeHeader = request.headers.get("Upgrade");
+      if (!upgradeHeader || upgradeHeader !== "websocket") {
+        return new Response("Durable Object expected Upgrade: websocket", {
+          status: 426,
+        });
+      }
+      // console.log("WebSocket Upgrade request received");
+      // This example will refer to the same Durable Object,
+      // since the name "foo" is hardcoded.
+      const name = path[1] ?? "foo";
+      let id = env.GAME_ROOM.idFromName(name);
+      let stub = env.GAME_ROOM.get(id);
+
+      return stub.fetch(request);
+    }
+
+    if (request.url.endsWith("/create")) {
+      const name = crypto.randomUUID();
+      // Handle the create game request
+      const id = env.GAME_ROOM.idFromName(name);
+      console.log("Creating game with ID:", id);
+      return new Response(
+        JSON.stringify({
+          id: id.toString(),
+        }),
+        {
+          status: 200,
+          statusText: "OK",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    }
+
+    return new Response(null, {
+      status: 400,
+      statusText: "Bad Request",
+      headers: {
+        "Content-Type": "text/plain",
+      },
+    });
+  },
+};
+>>>>>>> parent of 5788069 (nuke)
